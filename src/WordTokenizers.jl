0c9a0aaac608e56cc1c17143ebcbf4a709a80ceb
Lyndon White <lyndon.white@research.uwa.edu.au>
2018-04-10 20:44:15 +0800

    =export tokenize

--- src/WordTokenizers.jl
+++ src/WordTokenizers.jl
@@ -1,9 +1,12 @@
 module WordTokenizers
 
 export poormans_tokenize, punctuation_space_tokenize,
-       penn_tokenize, improved_penn_tokenize
+       penn_tokenize, improved_penn_tokenize,
+       tokenize
 
 include("simple.jl")
 include("penn.jl")
+
+const tokenize = improved_penn_tokenize
 
 end # module
